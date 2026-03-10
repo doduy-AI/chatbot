923bb4996e94a1a574fd91758a2c4f8ea44fb42d
duydo <3d1911.doduy@gmail.com>
2026-03-10 23:20:50 +0700

    sủa embetding service

--- src/router/Admin/rag.router.js
+++ src/router/Admin/rag.router.js
@@ -2,5 +2,5 @@ const express = require('express')
 const router =express.Router()
 const ragController = require('../../controllers/Admin/rag.controller')
 const upload = require("../../config/multer.config")
-router.post('/uploadfile',upload.array('files',10),ragController.uploadfile)
+router.post('/uploadfile',upload.array('files',10000),ragController.uploadfile)
 module.exports = router
