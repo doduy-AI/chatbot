fc6356ce6a97e2751d3e4b6e4242be89f833b252
doduy.bytehome <3d1911.doduy@gmail.com>
2026-03-18 14:23:41 +0700

    bỏ dấu , test

--- AI_Service/tts_service/testmodel1.py
+++ AI_Service/tts_service/testmodel1.py
@@ -127,12 +127,12 @@ def tts_batch(texts: list, language: str = "vi", output_dir: str = "output"):
 # THỰC THI CHƯƠNG TRÌNH
 # ==========================================
 texts = [
-    "Chào bạn! Mình là Emily.",
-    "Chúng mình có thể chơi ở hành tinh phiêu lưu hoặc vương quốc bong bóng xà phòng cho vui nhé!",
-    "Hoặc nếu bạn thích, mình có thể nhảy múa trên mây nữa đó!",
-    "Good evening nha! Chào bạn nhỏ! Mình là Emily, robot kể chuyện đêm khuya nhưng siêu hài hước!",
-    "Chào bé! Emily đây, mình biết mọi trò chơi vui trên đời, bạn muốn chơi trò nào trước nào?",
-    "Ồ hô hô! Xin chào! Mình là Emily, robot siêu hoạt bát, sẵn sàng nhảy múa cùng bạn ngay bây giờ!"
+    "Chào bạn Mình là Emily",
+    "Chúng mình có thể chơi ở hành tinh phiêu lưu hoặc vương quốc bong bóng xà phòng cho vui nhé",
+    "Hoặc nếu bạn thích mình có thể nhảy múa trên mây nữa đó",
+    "Good evening nha Chào bạn nhỏ! Mình là Emily robot kể chuyện đêm khuya nhưng siêu hài hước",
+    "Chào bé Emily đây mình biết mọi trò chơi vui trên đời bạn muốn chơi trò nào trước nào?",
+    "Ồ hô hô! Xin chào! Mình là Emily robot siêu hoạt bát sẵn sàng nhảy múa cùng bạn ngay bây giờ!"
 
 ]
 
