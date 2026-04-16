a116d8d7ac301c3cbe9e498bdaca40461968d363
duydo <3d1911.doduy@gmail.com>
2026-04-16 23:13:22 +0700

    delete file clien old and add funtion login_and_get_token for file main.py new

---
+++ client/main.py
@@ -0,0 +1,23 @@
+from login import Login
+import websocket
+AUTH = Login
+
+# class WebSocketHandler:
+#     def __init__(self):
+#         print("[WS] WEBSOCKET")
+#         self.ws = None
+#         self.username = "bytehome"
+#         self.password = "bytehome"
+#         self.connectted = False
+#         self.ws_url = AUTH.login_and_get_token(self.username,self.password)
+    
+#     def on_message(self, ws , message):
+
+#     def connect(self):
+#         self.ws = websocket.WebSocketApp(
+#             self.ws_url,
+
+#         )
+#         return self.ws
+    
+
