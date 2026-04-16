91220f3943a63056bbc1bab9b1c3c27ae1b12ee4
duydo <3d1911.doduy@gmail.com>
2026-04-17 00:19:44 +0700

    read data mic

---
+++ client/data_mic.py
@@ -0,0 +1,65 @@
+import pyaudio
+import threading
+import time
+
+class MicStreamer:
+    def __init__(self):
+        self.CHUNK = 320
+        self.FORMAT = pyaudio.paInt16
+        self.CHANNELS = 1
+        self.RATE = 16000
+
+        self.p = pyaudio.PyAudio()
+
+        self.stream = self.p.open(
+            format=self.FORMAT,
+            channels=self.CHANNELS,
+            rate=self.RATE,
+            input=True,
+            frames_per_buffer=self.CHUNK
+        )
+
+        self.is_running = True      
+        self.is_recording = False 
+
+    def start(self):
+        threading.Thread(target=self._loop, daemon=True).start()
+
+    def _loop(self):
+        print("[MIC] Mic ready...")
+
+        while self.is_running:
+            if self.is_recording:
+                data = self.stream.read(self.CHUNK, exception_on_overflow=False)
+                print(len(data))
+            else:
+                time.sleep(0.01)
+
+    def start_recording(self):
+        print("[MIC] Start Mic")
+        self.is_recording = True
+
+    def stop_recording(self):
+        print("[MIC] Stop recording")
+        self.is_recording = False
+
+    def shutdown(self):
+        self.is_running = False
+        self.stream.stop_stream()
+        self.stream.close()
+        self.p.terminate()
+
+if __name__ == "__main__":
+    MIC = MicStreamer()
+    MIC.start()
+    
+    time.sleep(1)
+    MIC.start_recording()
+    
+    try:
+        time.sleep(5) 
+    except KeyboardInterrupt:
+        pass
+        
+    MIC.stop_recording()
+    MIC.shutdown()
