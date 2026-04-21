b9f5bbf54b9268058ed628b53940f7552d43a56d
doduy1911 <3d1911.doduy@gmail.com>
2026-04-21 15:16:51 +0700

    add luồng body text

--- src/controllers/client/chat.controller.js
+++ src/controllers/client/chat.controller.js
@@ -1,9 +1,23 @@
 const redisService = require('../../services/redisService');
+const User = require('../../model/user.model')
+
 const chat = async(req,res)=>{
-    task = {
-        
-    }
-    console.log(req.user.id)
+        const userId = req.user.id
+        if (!userId) return res.status(400).json({ success: false, message: 'Thiếu userId!' });
+        const user = await User.findOne({
+            where:{
+                id:userId,
+            }
+        })
+        const userID = req.user.id
+        const groupId = user.groupId
+        const {  text, history} = req.body 
+        const job ={
+           userID,
+           groupId,
+           text
+        } 
+        console.log(job)
 }
 
 module.exports = {chat}
