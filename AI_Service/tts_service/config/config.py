c8f778564ffea5cf56dc728a54f0c3dbaf24ff4b
doduy1911 <3d1911.doduy@gmail.com>
2026-04-22 18:03:04 +0700

    add pass redis tts_service

--- AI_Service/tts_service/config/config.py
+++ AI_Service/tts_service/config/config.py
@@ -4,12 +4,13 @@ BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 class Settings(BaseSettings):
     MODEL_NAME : str = "MODEL_NAME" ,
     TOKEN_HF : str = "TOKEN_HF",
-    REDIS_HOST: str = "REDIS_HOST"
-    REDIS_PORT: int = "REDIS_PORT" 
-    EXTERNAL_HOST: str = "EXTERNAL_HOST" 
-    QUEUE_MAXSIZE: int = "QUEUE_MAXSIZE" 
-    STREAM_GET_TIMEOUT: float = "STREAM_GET_TIMEOUT" 
-    QUEUE_PUT_TIMEOUT : float = "QUEUE_PUT_TIMEOUT"
+    REDIS_HOST: str = "REDIS_HOST",
+    REDIS_PORT: int = "REDIS_PORT" ,
+    EXTERNAL_HOST: str = "EXTERNAL_HOST" ,
+    QUEUE_MAXSIZE: int = "QUEUE_MAXSIZE" ,
+    STREAM_GET_TIMEOUT: float = "STREAM_GET_TIMEOUT" ,
+    QUEUE_PUT_TIMEOUT : float = "QUEUE_PUT_TIMEOUT",
+    PASS_REDIS : str = "PASS_REDIS",
     PORT : int = "PORT"
     
     model_config = SettingsConfigDict(
