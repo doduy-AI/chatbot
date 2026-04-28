c736ace7af16cdc4c139ad4284230b14ea32511e
doduy1911 <3d1911.doduy@gmail.com>
2026-04-28 18:45:15 +0700

    add list summary prompt

--- src/router/Admin/prompt.router.js
+++ src/router/Admin/prompt.router.js
@@ -4,6 +4,8 @@ const promptController = require('../../controllers/Admin/promt.controller')
 const SummaryPromptController = require('../../controllers/Admin/summaryPrompt.controller')
 router.patch('/edit/:id',promptController.edit)
 router.get('/group',promptController.group)
-router.post('/SummaryPrompt',SummaryPromptController.create)
+router.post('/SummaryPrompt/create',SummaryPromptController.create)
+router.patch('/SummaryPrompt/edit/:promptId',SummaryPromptController.edit)
+router.get("/SummaryPrompt/list",SummaryPromptController.list)
 
 module.exports = router
